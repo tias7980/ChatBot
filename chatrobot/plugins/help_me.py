e0230b9530e1cbf3a8636ab72d6c2c4dae2a5c16
Dev-v2 <89397215+New-Dev3@users.noreply.github.com>
2022-01-17 20:32:21 +0530

    We don't need fking image

--- chatrobot/plugins/help_me.py
+++ chatrobot/plugins/help_me.py
@@ -60,7 +60,7 @@ async def help(event):
             "You Can Deploy Your Own ChatBot By Watching Video Down There. \nThank You For Contacting Me.",
             buttons=[
                 [Button.url("Deploy Tutorial 📺", "t.me/Infotel14")],
-                [Button.url("Need Help ❓", "t.me/RGamer_s")],
+                [Button.url("Need Help ❓", "t.me/xGamer_s")],
             ],
         )
         
