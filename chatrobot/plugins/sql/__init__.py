56fbe1d90aa44215ab8e3a809f0a5791272bf2dc
WarnerStark <StarkGangZ@gmail.com>
2022-05-02 14:30:35 +0530

    Update __init__.py

--- chatrobot/plugins/sql/__init__.py
+++ chatrobot/plugins/sql/__init__.py
@@ -4,10 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from chatrobot import Config
+DB_URL = Config.DB_URL
 
+if DB_URL and DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1) 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DB_URL)
+    engine = create_engine(DB_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
